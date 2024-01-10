4e0c3137a4b9d930767305e70476c52981bc62ec
Jakub Cywka <jakub.ryszard.cywka@gmail.com>
2024-01-10 12:35:00 +0100

    Init ability actor info

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -16,6 +16,12 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 	GENERATED_BODY()
 public:
 	AAuraEnemy();
+
+	/** Enemy interface */
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
+	/** End enemy interface */
+	
+protected:
+	virtual void BeginPlay() override;
 };
