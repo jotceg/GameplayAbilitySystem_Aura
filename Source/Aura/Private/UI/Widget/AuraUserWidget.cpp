1f60125028d821d13fec62da526dccc218ae9a43
Jakub Cywka <jakub.ryszard.cywka@gmail.com>
2024-01-12 14:02:56 +0100

    Aura user widget and widget controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Jakub Cywka @jotceg
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
