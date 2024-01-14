a0abf778d271bb2f6edff1ab909fff0ae61b3048
Jakub Cywka <jakub.ryszard.cywka@gmail.com>
2024-01-14 23:03:56 +0100

    Overlay widget controller

--- Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -3,3 +3,14 @@
 
 #include "UI/WidgetController/AuraWidgetController.h"
 
+FWidgetControllerParams::FWidgetControllerParams()
+{
+}
+
+void UAuraWidgetController::SetWidgetControllerParams(const FWidgetControllerParams& WCParams)
+{
+	PlayerController = WCParams.PlayerController;
+	PlayerState = WCParams.PlayerState;
+	AbilitySystemComponent = WCParams.AbilitySystemComponent;
+	AttributeSet = WCParams.AttributeSet;
+}
